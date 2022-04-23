50ec60c4d5b76e5fc22513b60e9c965d7cafb6a8
Mike Hernandez <mhernandez@guidemarkhealth.com>
2022-04-23 08:24:10 -0400

    init

---
+++ container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { mount } from 'marketing/MarketingApp';
+
+import MarketingApp from './components/MarketingApp'
+
+console.log('mount', mount)
+
+export default () => {
+    return <div>
+    <h1>Container Hi there!</h1>
+    <MarketingApp />
+    </div>
+}
